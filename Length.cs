3a1d94ead9434bf2ff58918b915e7aac057f96ab
farhinsheikh <farhinsheikh2015@gmail.com>
2021-04-15 21:24:09 +0530

    [Farhin]ADDED:first commit

---
+++ Length.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComparisionProblem
+{
+    class Length
+    {
+        int x1, x2, y1, y2;
+        public Length(int x1, int x2, int y1, int y2)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+        public double CalculateLength()
+        {
+            double length = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            return length;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LineComparisionProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison problem");
+            Length line = new Length(8, 4, 6, 1);
+            double length = line.CalculateLength();
+            Console.WriteLine("Length of line is = " + length);
+        }
+    }
+}
