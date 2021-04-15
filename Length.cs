0e5c53a5b487aa7d949869b5c611dbe2f530bde0
farhinsheikh <farhinsheikh2015@gmail.com>
2021-04-15 21:34:58 +0530

    [Farhin]ADDED:Calculate length

--- Length.cs
+++ Length.cs
@@ -10,8 +10,8 @@ namespace LineComparisionProblem
         public Length(int x1, int x2, int y1, int y2)
         {
             this.x1 = x1;
-            this.y1 = y1;
             this.x2 = x2;
+            this.y1 = y1;
             this.y2 = y2;
         }
         public double CalculateLength()
